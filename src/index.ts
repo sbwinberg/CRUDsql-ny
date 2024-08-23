9b7d8546fcc48ca64412334410078a17a6a854b5
David Heidari <davidheidari2@gmail.com>
2024-08-23 15:45:49 +0200

    adding all data

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import { error } from "console";
+import express, { Response, Request, NextFunction } from "express";
+import { Pool, Client } from "pg";
+
+const app = express();
+
+// interface poolConfig {
+//   user: string;
+//   host: string;
+//   database: string;
+//   password: string;
+//   port: number;
+// }
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: parseInt(process.env.PORT || "5432"),
+});
+
+async () => {
+  const client = await pool.connect();
+  try {
+    const { rows } = await client.query("Select current_user");
+    const currentUser = rows[0]["current_user"];
+    console.log(currentUser);
+  } catch {
+    console.log(error);
+  } finally {
+    client.release();
+  }
+};
+
+const SERVER_PORT = process.env.SERVER_PORT || 1337;
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Jensa bajsar");
+});
+
+app.post("/posts", (req: Request, res: Response, next: NextFunction) => {
+  const {
+    body,
+    params: { id },
+  } = req.body;
+});
+
+app.put("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Jensa bajsar");
+});
+
+app.patch("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Jensa bajsar");
+});
+
+app.delete("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Jensa bajsar");
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log("Server started on: " + SERVER_PORT);
+});
