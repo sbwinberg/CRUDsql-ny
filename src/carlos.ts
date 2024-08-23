8feaf77052f5ea6069ad7e13892cdbf840761ba8
David Heidari <davidheidari2@gmail.com>
2024-08-23 23:28:52 +0200

    commented carlos.ts for now

--- src/carlos.ts
+++ src/carlos.ts
@@ -1,19 +1,19 @@
-import { Router } from "express";
-import pool from "../db";
+// import { Router } from "express";
+// import pool from "../db";
 
-const router = Router();
+// const router = Router();
 
-router.post("/users", async (req, res) => {
-  const { name, email, adress } = req.body;
-  try {
-    const newUser = await pool.query(
-      "INSERT INTO users (name, email, adress) VALUES ($1, $2, $3)(ny user) RETURNING *",
-      [name, email, adress]
-    );
-    res.json(newUser.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
+// router.post("/users", async (req, res) => {
+//   const { name, email, adress } = req.body;
+//   try {
+//     const newUser = await pool.query(
+//       "INSERT INTO users (name, email, adress) VALUES ($1, $2, $3)(ny user) RETURNING *",
+//       [name, email, adress]
+//     );
+//     res.json(newUser.rows[0]);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
 
-export default router;
+// export default router;
