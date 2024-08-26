9cdc470538d7ae1c88eefe015cac4df0a0b1279f
David Heidari <davidheidari2@gmail.com>
2024-08-26 16:31:51 +0200

    added delet

--- src/carlos.ts
+++ src/carlos.ts
@@ -36,10 +36,38 @@ router.put("/put/:id", async (req, res) => {
   }
 });
 
-router.patch("/post/:id", async (req, res) => {
-  const { body } = req.body;
+router.patch("/patch/:id", async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const { user_name, email } = req.body;
+
+  try {
+    const newUser = await pool.query(
+      'UPDATE "user" SET user_name = $1, email = $2 WHERE user_id = $3 RETURNING*',
+      [user_name, email, id]
+    );
+
+    res.json(newUser.rows[0]);
+  } catch (error: any) {
+    console.error(error.message);
+  }
 });
 
-router.delete("/post/:id", async (req, res) => {});
+router.delete("/delete/:id", async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+
+  try {
+    const newUser = await pool.query(
+      'DELETE FROM "user" WHERE user_id = $1 RETURNING*',
+      [id]
+    );
+    res.json(newUser.rows[0]);
+  } catch (error: any) {
+    console.error(error.message);
+  }
+});
 
 export default router;
