0389c90046c09e6927af1721eeaeb2f0adc81862
David Heidari <davidheidari2@gmail.com>
2024-08-26 16:12:18 +0200

    added put

--- src/carlos.ts
+++ src/carlos.ts
@@ -19,4 +19,27 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.put("/put/:id", async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+
+  const { user_name, email } = req.body;
+  try {
+    const newUser = await pool.query(
+      'UPDATE "user" SET user_name = $1, email = $2 WHERE user_id = $3 RETURNING*',
+      [user_name, email, id]
+    );
+    res.json(newUser.rows[0]);
+  } catch (error: any) {
+    console.error(error.message);
+  }
+});
+
+router.patch("/post/:id", async (req, res) => {
+  const { body } = req.body;
+});
+
+router.delete("/post/:id", async (req, res) => {});
+
 export default router;
