17086e4aa0045e955e4ec3d8cf11f6665f2bfe42
Jens Bengtsson <jb225je@student.lnu.se>
2024-08-28 14:42:52 +0200

    user patch now work as expected

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,16 +4,16 @@ import pool from "../database/db";
 const router = Router();
 
 router.post("/user", async (req: Request, res: Response): Promise<void> => {
-  const { user_name, email, user_name2, email2 } = req.body;
+  const { user_name, email } = req.body;
 
   try {
     const newUser = await pool.query(
-      'INSERT INTO "user" (user_name , email) VALUES ($1, $2),($3, $4) RETURNING *',
-      [user_name, email, user_name2, email2]
+      'INSERT INTO "user" (user_name , email) VALUES $1, $2 RETURNING *',
+      [user_name, email]
     );
     res.json(newUser.rows[0]);
-  } 
-  
+  }
+
   catch (error: any) {
     console.error(error.message);
   }
@@ -37,19 +37,42 @@ router.put("/user/:id", async (req: Request, res: Response): Promise<void> => {
   }
 });
 
+interface userDatabase {
+  user_name: string;
+  email: string;
+}
+
 // Det går inte att uppdatera en , den andra blir null, fungerar som put just nu
 router.patch("/user/:id", async (req: Request, res: Response): Promise<void> => {
+  // hämtar user med id, sparar i lokal variable
+  let current_user;
   const {
     params: { id },
   } = req;
-  const { user_name, email } = req.body;
+  let { user_name, email } = req.body;
+
+  // get the current user
+  try {
+    const result = await pool.query(
+      'SELECT * FROM "user" WHERE user_id = $1',
+      [id]
+    );
+    current_user = result.rows[0]
+
+    // checkes if the client has updated data for user_name and/or email if not then asign the data from current_user
+    if (!user_name) user_name = current_user.user_name;
+    if (!email) email = current_user.email;
+  } catch (error: any) {
+    console.log(current_user, 'current_user catch');
+    console.error(error.message);
+  }
+
 
   try {
     const newUser = await pool.query(
       'UPDATE "user" SET user_name = $1, email = $2 WHERE user_id = $3 RETURNING*',
       [user_name, email, id]
     );
-
     res.json(newUser.rows[0]);
   } catch (error: any) {
     console.error(error.message);
