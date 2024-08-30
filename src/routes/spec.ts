50a77b0b24c1712052a2f0a93e1221dc61458030
Jens Bengtsson <jb225je@student.lnu.se>
2024-08-30 13:26:39 +0200

    spec2

--- src/routes/spec.ts
+++ src/routes/spec.ts
@@ -29,4 +29,56 @@ router.get(
   }
 );
 
-export {router as specRouter};
+// Hämta alla användare som har fler än tre inlägg.
+router.get(
+  "/spec2",
+  async (
+    req: Request,
+    res: Response<CreateUserResponse[] | CreatePostResponse[] | { error: string }>,
+    next: NextFunction
+  ): Promise<void> => {
+
+    try {
+      const postCounter = await pool.query(
+        `SELECT "user".user_name, COUNT(post.post_user_id) AS number_of_posts
+          FROM "user"
+          LEFT OUTER JOIN post
+          ON post.post_user_id = "user".user_id
+          GROUP BY "user".user_id
+          HAVING COUNT(post.post_user_id) > 3
+        `
+      );
+      res.send(postCounter.rows);
+    } catch (error: unknown) {
+      next(error);
+    }
+  }
+);
+
+// Hämta de senaste 5 inläggen från varje användare.
+router.get(
+  "/spec3",
+  async (
+    req: Request,
+    res: Response<CreateUserResponse[] | CreatePostResponse[] | { error: string }>,
+    next: NextFunction
+  ): Promise<void> => {
+
+    try {
+      const postCounter = await pool.query(
+        `SELECT "user".user_name, COUNT(post.post_user_id) AS number_of_posts
+        FROM "user"
+        LEFT OUTER JOIN post
+        ON post.post_user_id = "user".user_id
+        GROUP BY "user".user_id
+        `
+      );
+      res.send(postCounter.rows);
+    } catch (error: unknown) {
+      next(error);
+    }
+  }
+);
+
+
+export { router as specRouter };
