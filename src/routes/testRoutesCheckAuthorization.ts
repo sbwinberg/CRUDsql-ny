d7a20146bf666d7329e2ed8719810b546811e764
Jens Bengtsson <jb225je@student.lnu.se>
2024-09-16 12:40:27 +0200

    added some loggs

--- src/routes/testRoutesCheckAuthorization.ts
+++ src/routes/testRoutesCheckAuthorization.ts
@@ -7,24 +7,28 @@ const router = express.Router();
 
 // Only user have access to this route
 router.get("/user-only", authorize("user"), (req, res) => {
+  console.log('user-only route');
   res.json({ message: "Welcome user" });
 });
 
 // Only admins have access to this route
 router.get("/admin-only", authorize("admin"), (req, res) => {
+  console.log('admin-only route');
   res.json({ message: "Welcome Admin" });
 });
 
 
 // Admins and editors have access to this route
 router.get("/content-managment", authorize("admin", "editor"), (req, res) => {
+  console.log('content-managment route');
   res.json({ message: "Welcome to content managment" });
 });
 
 // All roles have access to this route
 router.get("/user-profile", authorize("admin", "editor", "user"), (req, res) => {
-    res.json({ message: "Welcome to your profile" });
-  }
+  console.log('user-profile route');
+  res.json({ message: "Welcome to your profile" });
+}
 );
 
-export {router as checkAuthRoutes};
+export { router as checkAuthRoutes };
