05b27456b14e76e480ad0f6e36d117d2be538d08
David Heidari <davidheidari2@gmail.com>
2024-10-04 10:45:00 +0200

    add a backend folder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,16 +6,18 @@ import session from "express-session";
 //routes
 import { campaignRoutes } from "./routes/campaign";
 import authRouter from "./routes/auth";
-import {router as userRouter} from "./routes/user"
+import { router as userRouter } from "./routes/user";
 
 const app = express();
 
 // gitHubStrategy
-app.use(session({
-  secret: 'hemligt',
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "hemligt",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -26,18 +28,17 @@ passport.serializeUser(function (user, done) {
 
 passport.deserializeUser(function (obj: any, done) {
   done(null, obj);
-})
+});
 
 // ROUTES
 app.use(errorHandler);
 app.use(express.json());
 app.use("/campaign", campaignRoutes);
-app.use("/users", userRouter)
-app.use("/auth/github", authRouter)
+app.use("/users", userRouter);
+app.use("/auth/github", authRouter);
 
 const SERVER_PORT = process.env.SERVER_PORT || 1337;
 
 app.listen(SERVER_PORT, () => {
   console.log("Server started on: " + SERVER_PORT);
 });
-
