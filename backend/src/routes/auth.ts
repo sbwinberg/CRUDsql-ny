a9256c988c561570d2f0d4d674edc7048e4a47ee
Oscar Donaldson <oscar@donaldson.se>
2024-10-17 10:19:37 +0200

    start on logout

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,24 +1,40 @@
 import { Router } from "express";
 import passport from "passport";
-import '../authStrategies/githubStrategy';
-import { Request, Response } from 'express';
+import "../authStrategies/githubStrategy";
+import { Request, Response } from "express";
 
 const app = Router();
 
-app.get('/',
-    passport.authenticate('github'));
+app.get("/", passport.authenticate("github"));
 
-app.get('/callback',
-    passport.authenticate('github', { failureRedirect: '/' }),
-    function (req: Request, res: Response) {
-        res.redirect('/profile');
-    });
+app.get(
+  "/callback",
+  passport.authenticate("github", { failureRedirect: "/" }),
+  function (req: Request, res: Response) {
+    // res.redirect('/profile');
+    // Indikerar att användaren är inloggad
+    res.redirect("http://localhost:5173/?loggedIn=true");
+  }
+);
 
-app.get('/profile', (req: Request, res: Response) => {
-    if (!req.isAuthenticated()) {
-        return res.redirect('/auth/github');
-    }
-    res.json(req.user);
+app.get("/profile", (req: Request, res: Response) => {
+  if (!req.isAuthenticated()) {
+    return res.redirect("/auth/github");
+  }
+  res.json(req.user);
 });
+
+// ---------------------------------------------------------------------------
+// Utloggningsrutt
+app.get("/logout", (req: Request, res: Response) => {
+  req.logout((err) => {
+    if (err) {
+      console.error("Error during logout:", err);
+      return res.status(500).json({ message: "Error logging out" });
+    }
+    res.redirect("http://localhost:5173/");
+  });
+});
+// ---------------------------------------------------------------------------
 
 export default app;
