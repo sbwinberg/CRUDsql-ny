7bf98e81521b0972590e3a8b5302d30583ae2a17
Jens Bengtsson <jb225je@student.lnu.se>
2024-10-18 16:19:52 +0200

    changed file structure

--- frontend/src/routes/login/LoginPage.tsx
+++ frontend/src/routes/login/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { LoginForm } from "@/components/Loginform";
+import { LoginForm } from "@/components/LoginForm";
 
 export const LoginPage = () => {
   return (
